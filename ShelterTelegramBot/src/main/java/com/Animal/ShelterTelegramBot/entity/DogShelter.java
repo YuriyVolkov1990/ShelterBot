3e5105cccb9b585c55fee16a1c8b6c1935b6f6b8
Professional <new99kpicem@gmail.com>
2023-10-29 16:15:36 +0300

    ura_work

---
+++ ShelterTelegramBot/src/main/java/com/Animal/ShelterTelegramBot/entity/DogShelter.java
@@ -0,0 +1,40 @@
+package com.Animal.ShelterTelegramBot.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "dog_shelter")
+@Getter
+@Setter
+@AllArgsConstructor
+public class DogShelter implements Shelter{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name")
+    private String name;
+
+    @Override
+    public String getInfo() {
+        return null;
+    }
+
+    @Override
+    public String getGuardData() {
+        return null;
+    }
+
+    @Override
+    public String getRecommendation() {
+        return null;
+    }
+
+    @Override
+    public String getContact() {
+        return null;
+    }
+}
---
+++ ShelterTelegramBot/src/main/java/com/Animal/ShelterTelegramBot/entity/Shelter.java
@@ -0,0 +1,8 @@
+package com.Animal.ShelterTelegramBot.entity;
+
+public interface Shelter {
+    String getInfo();
+    String getGuardData();
+    String getRecommendation();
+    String getContact();
+}
